3b92faee1fce50d41e22bd80d06bf8f0b3dab4ee
Christian Saxton <CRS-MACBOOK@Christians-MacBook-Pro.local>
2018-04-20 12:26:43 -0400

    Functional push. Still working on download button mechanics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,8 @@ var animals = ['cat', 'dog', 'fish', 'bird', 'rat', 'hamster', 'chinchilla', 'ho
     btns = $('#buttons'),
     gifs = $('#gifs'),
     main = $('body'),
+    offset = 0,
+    query = '';
     giphyToken = 'rJt32RsvjUdHPIBRNZadxmdnnsGuvEMh';
 
 
@@ -33,32 +35,34 @@ function buttonGen () {
 // Initial button generation
 buttonGen();
 
-// When animal button clicked
-$(main).on('click', '.animal-btn', function() {
-    // Query GIPHY API
-    var query = $(this).attr('data-animal');
-    $.ajax({url:'https://api.giphy.com/v1/gifs/search?q='+query+'&limit=10&api_key='+giphyToken, method: 'GET'})
+// Query GIPHY API
+function giphyAPI() {
+    $.ajax({url:'https://api.giphy.com/v1/gifs/search?q='+query+'&limit=10&offset='+offset+'&api_key='+giphyToken, method: 'GET'})
         .done(function(response){
             console.log(response);
             for(i=0;i<10;i++) {
                 // Create outer div, image, and rating div
                 var newBox = $('<div class="wf-box">'),
                     newContent = $('<div class="content">'),
-                    newImg = $('<img class="gif">'),
+                    newGif = $('<video class="gif">'),
+                    newDL = $('<img src="assets/images/download.png" class="download">'),
+                    newLink = $('<a href="'+response.data[i].images.original.url+'">'),
                     newH3 = $('<h3>');
 
                 // Hierarchy adjustments
                 newBox.append(newContent);
-                newContent.append(newImg, newH3);
+                newLink.append(newDL);
+                newContent.append(newGif, newLink, newH3);
 
                 // Set image source and id
-                newImg.attr({'src': response.data[i].images.original_still.url, 'id': response.data[i].id, 'data-play':'false'});
+                newGif.attr({'src': response.data[i].images.looping.mp4, 'id': response.data[i].id, 'data-play':'false'});
 
-                // Add default text to H3
+                // Add rating text to H3
                 newH3.text('Rating: '+ response.data[i].rating.toUpperCase());
 
                 // Append to images area
                 gifs.append(newBox);
+
             }
 
             // Activate waterfall layout
@@ -69,7 +73,28 @@ $(main).on('click', '.animal-btn', function() {
                 minBoxWidth: 250
             });
 
+            // Show more button
+            $('#btn-holder').css('display', 'block');
         });
+}
+
+// When animal button clicked
+$(main).on('click', '.animal-btn', function() {
+    // Clear any old gifs
+    $('#gifs').html('<div id="btn-holder"><img src="assets/images/more.png" id="btn-more"></div>');
+
+    // Set query
+    query = $(this).attr('data-animal');
+
+    // Ajax call to API
+    giphyAPI();
+
+    // Add more button spacer
+    $('#left-col').append('<div id="btn-spacer"></div>');
+
+    // Add to offset
+    offset += 10;
+    
 });
 
 // When thumbnail clicked
@@ -79,26 +104,58 @@ $(main).on('click', '.gif', function() {
     
     if (play == 'false') {
     // Change from thumb to gif
-        $(this).attr('src', 'https://media0.giphy.com/media/'+id+'/giphy.gif');
+        $(this).trigger('play');
         $(this).attr('data-play', 'true');
     }
     else if (play == 'true') {
     // Change from gif to thumb
-        $(this).attr('src', 'https://media0.giphy.com/media/'+id+'/giphy_s.gif');
+        $(this).trigger('pause');
         $(this).attr('data-play', 'false');
     }
 });
+
+// When more button clicked
+$(main).on('click', '#btn-more', function(){
+
+    // Query GIPHY
+    giphyAPI();
+
+    // // Scroll to bottom of window
+    // var interval = setInterval(scroll, 1000);
+    // var scrollY = window.pageYOffset;
+    // console.log(scrollY);
+    // function scroll() {
+    //     if (scrollY == 0) {
+    //         clearInterval(interval);
+    //         window.scrollTo(0,document.body.scrollHeight);
+    //     } else {
+    //     window.scrollTo(0,document.body.scrollHeight);
+    //     }
+    // }
+
+    // Add to offset
+    offset += 10;
+});
+
+// Show download button
+$(main).on('mouseover','.content', function (){
+    $(this).find(".download").css('opacity','0.8');
+});
+
+// Hide download button
+$(main).on('mouseout','.content', function (){
+    $(this).find(".download").css('opacity','0');
+});
+
 
 // When "Add Animal" button clicked
 $(main).on('click', '#add-animal', function() {
 
     // Assign text box value to variable
     var newAnimal = $('#new-animal').val().trim();
-    console.log(newAnimal);
 
     // Add new animal to animals array
     animals.push(newAnimal);
-    console.log(animals);
 
     // Clear text box
     $('#new-animal').val('');
